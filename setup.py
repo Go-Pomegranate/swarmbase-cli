6cbe28b9e0abbdb9d3fd4a8b96cb876338157bfd
Eryk Panter <eryk.panter@contractors.roche.com>
2024-10-09 17:51:07 +0200

    feat: orchestrate pypi distribution pipeline

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="swarmbase-cli",
-    version="0.1.0",
+    version="0.1.0-alpha",
     packages=find_packages(),
     install_requires=[
         "click",
@@ -16,4 +16,6 @@ setup(
     author="swarmbase.ai",
     author_email="eryk.panter@swarmbase.ai",
     description="A CLI for interacting with the swarmbase.ai API",
+    long_description=open('README.md').read(),  # Ensure this file exists and is correctly formatted
+    long_description_content_type='text/markdown',  # Change to 'text/x-rst' if using reST
 )
