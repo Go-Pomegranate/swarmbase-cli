8c1e8f52e14324f9637beda4c71320e37d8b9ecf
Eryk Panter <eryk.panter@contractors.roche.com>
2024-10-09 19:00:28 +0200

    feat: orchestrate pypi distribution pipeline

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="swarmbase-cli",
-    version="0.1.0-alpha",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
         "click",
