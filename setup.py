ba9c7606fdfd020c8c985169dd32ba86daf4265f
Eryk Panter <eryk.panter@contractors.roche.com>
2024-10-09 17:29:49 +0200

    feat: orchestrate pypi distribution pipeline

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="swarm",
+    name="swarmbase-cli",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
@@ -12,7 +12,7 @@ setup(
         [console_scripts]
         swarm=cli:cli
     """,
-    url="https://github.com/yourusername/swarm-cli",  # Zaktualizuj ten URL
+    url="https://github.com/Go-Pomegranate/swarmbase-cli",  # Zaktualizuj ten URL
     author="swarmbase.ai",
     author_email="eryk.panter@swarmbase.ai",
     description="A CLI for interacting with the swarmbase.ai API",
